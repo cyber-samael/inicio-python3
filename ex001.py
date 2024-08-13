dd200a79709e87fb9f4ed866fea71c3b026a9493
samael-cyber <cybermachinebot@gmail.com>
2024-08-12 21:11:17 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,72 @@
+# anotações ( separadores com # )
+# tudo que estiver com apenas 1 # é EXERCICIOS
+# Tudo que estiver com 2 #, é DICA
+# Tudo que estiver com 3 #, função basics ou para saber
+# Tudo que estiver com 4 #, é importante, ESTUDE !!!!
+#================================================================
+
+# EX001
+
+# print('Hello World!')
+
+#===========================================================
+
+# EX002
+### nome = input(str('Qual o seu nome? '))
+
+#### print('Olá',nome,'é um prazer estar aqui com você')
+## utilizamos a maneira a cima ou a maneira abaixo
+#### print('Olá, é um prazer estar aqui com voce,{}'.format(nome))
+
+#======================================================================================================================================================
+
+# exemplo01
+
+##n1 = int(input('Digite um número: '))
+##n2 = int(input('Digite outro: '))
+##n3 = int(input('Digite outro: '))
+##s = n1 + n2 + n3
+
+##print('A soma total entre os números é: {}'.format(s))
+
+
+#print('░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░████████╗░█████╗░██████╗░')
+#print('██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗')
+#print('██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║░░░██║░░░██║░░██║██████╔╝')
+#print('██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║░░░██║░░░██║░░██║██╔══██╗')
+#print('╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║░░░██║░░░╚█████╔╝██║░░██║')
+#print('░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝')
+
+
+#soma1 = int(input('Digite o primeiro valor da soma: '))
+#soma2 = int(input('Digite o segundo valor da soma: '))
+#soma3 = int(input('Digite o terceiro valor da soma: '))
+
+
+#menos1 = int(input('Agora digite o valor para a subtração: '))
+#menos2 = int(input('Digite o segundo valor para a subtração: '))
+#menos3 = int(input('Digite o terceiro valor para a subtração: '))
+
+
+#mult1 = int(input('Digite o primeiro valor para multiplicação: '))
+##mult2 = int(input("Digite o segundo valor para multipliação: "))
+
+
+#div1 = float(input('Digite o primeiro valor da divisão: '))
+#div2 = float(input('Digite o segundo valor da divisão: '))
+
+
+#print('O valor total da soma é: {}'.format(soma1 + soma2 + soma3))
+
+#print('O valor total da subtração é: {}'.format(menos1 - menos2 - menos3))
+
+#print('O valor total da multiplicação é: {}'.format(mult1 * mult2))
+
+#print('O valor da divisão é: {}'.format(div1/div2))
+
+#============================================================================================================================================================================
+
+
+# EX003
+
+
